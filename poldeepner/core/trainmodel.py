d81aa0cce448cfe92102363962246b05eaa3c723
gawor <gawor@156.17.135.10>
2019-03-12 14:02:36 +0000

    added gpu nb argument, possibility of running on multiple gpus

--- poldeepner/core/trainmodel.py
+++ poldeepner/core/trainmodel.py
@@ -14,11 +14,16 @@ parser.add_argument('-m', required=True, metavar='PATH', help='path to a folder 
 parser.add_argument('-n', required=True, metavar='nn_type', help='type of NN: GRU or LSTM', default='GRU')
 parser.add_argument('-e', required=True, default=32, type=int, metavar='num', help='number of epoches')
 parser.add_argument('-s', required=False, default=300, type=int, metavar='num', help='size of the input')
+parser.add_argument('-g', required=True, inargs='+', help='which GPUs to use')
 
 args = parser.parse_args()
 model = args.m
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+gpus = ''
+for gpu_nb in args.g:
+    gpus += str(gpu_nb) + ' '
+
+os.environ["CUDA_VISIBLE_DEVICES"] = gpus
 print(device_lib.list_local_devices())
 
 if not os.path.exists(model):
@@ -36,3 +41,4 @@ print("Test : %d" % len(x_test))
 m = Sequence(args.f, use_char=False, nn_type=args.n, input_size=args.s)
 m.fit(x_train, y_train, args.f, x_test, y_test, epochs=args.e, batch_size=32)
 m.save(model_weights, model_params, model_preprocessor)
+
