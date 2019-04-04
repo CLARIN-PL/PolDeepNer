e21d2575b76cebc24fbeee987509709b4b265271
root <michal.gawor@pwr.edu.pl>
2019-04-04 18:33:14 +0000

    test added

---
+++ test.py
@@ -0,0 +1,11 @@
+from process_file import process_file
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Process IOB file, recognize NE and save the output to another IOB file.')
+parser.add_argument('-i', required=True, metavar='PATH', help='input IOB file')
+
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    process_file(args.i, '/test_result.iob')
